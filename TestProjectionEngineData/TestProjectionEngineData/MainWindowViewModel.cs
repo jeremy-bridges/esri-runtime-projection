7fa24feaebd1b9ffe40a34372ef66748b22dbc8d
Jeremy Bridges <jeremy.bridges@se.com>
2022-09-19 12:07:20 -0600

    Ask for PE directory at app start. Find initial replica from same directory

--- TestProjectionEngineData/TestProjectionEngineData/MainWindowViewModel.cs
+++ TestProjectionEngineData/TestProjectionEngineData/MainWindowViewModel.cs
@@ -1,70 +1,101 @@
 ﻿using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
-using Microsoft.Win32;
 using System;
-using System.Windows;
+using System.ComponentModel;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Windows.Input;
+using MessageBox = System.Windows.MessageBox;
 
 namespace TestProjectionEngineData
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : INotifyPropertyChanged
     {
         private readonly Action<Envelope> _zoom;
+        private Map _map;
 
         public MainWindowViewModel(Action<Envelope> zoom)
         {
             _zoom = zoom;
-            TransformationCatalog.ProjectionEngineDirectory = @"C:\pedata";
-            Map = new Map(new Basemap(new ArcGISTiledLayer(new Uri(
-                "https://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer"))));
             AddReplicaCommand = new SimpleCommand(AddReplica);
+
+            MapInit();
         }
 
-        public Map Map { get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Map Map 
+        {
+            get => _map;
+            set
+            {
+                _map = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Map)));
+            }
+        }
 
         public ICommand AddReplicaCommand { get; }
+
+        private async Task MapInit()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(3));
+
+            var folderDialog = new FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                TransformationCatalog.ProjectionEngineDirectory = folderDialog.SelectedPath;
+                Map = new Map(new Basemap(new ArcGISTiledLayer(new Uri(
+                    "https://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer"))));
+                
+                foreach (var file in Directory.GetFiles(folderDialog.SelectedPath, "*.geodatabase"))
+                {
+                    await AddReplica(file);
+                }
+
+                _zoom(new Envelope(
+                    x1: 400177.58867753856,
+                    y1: 650887.0249267563,
+                    x2: 400367.92690205923,
+                    y2: 650988.133270391,
+                    SpatialReference.Create(27700)));
+            }
+        }
 
         private void AddReplica()
         {
             var dialog = new OpenFileDialog();
-            if (dialog.ShowDialog() ?? false)
+            if (dialog.ShowDialog() == DialogResult.OK)
             {
-                Geodatabase.OpenAsync(dialog.FileName).ContinueWith(t =>
+                AddReplica(dialog.FileName);
+            }
+        }
+
+        private async Task AddReplica(string gdbPath)
+        {
+            try
+            {
+                var gdb = await Geodatabase.OpenAsync(gdbPath);
+
+                foreach (var table in gdb.GeodatabaseFeatureTables)
                 {
-                    if (t.IsCanceled)
-                    {
-                        MessageBox.Show("Geodatabase open cancelled");
-                        return;
-                    }
-                    if (t.IsFaulted)
-                    {
-                        MessageBox.Show($"Geodatabase open failed: {t.Exception.Message}");
-                        return;
-                    }
+                    Map.OperationalLayers.Add(new FeatureLayer(table));
+                }
 
-                    foreach (var table in t.Result.GeodatabaseFeatureTables)
-                    {
-                        Map.OperationalLayers.Add(new FeatureLayer(table));
-                    }
+                foreach (var dimTable in gdb.GeodatabaseDimensionTables)
+                {
+                    Map.OperationalLayers.Add(new DimensionLayer(dimTable));
+                }
 
-                    foreach (var dimTable in t.Result.GeodatabaseDimensionTables)
-                    {
-                        Map.OperationalLayers.Add(new DimensionLayer(dimTable));
-                    }
-
-                    foreach (var annoTable in t.Result.GeodatabaseAnnotationTables)
-                    {
-                        Map.OperationalLayers.Add(new AnnotationLayer(annoTable));
-                    }
-
-                    _zoom(new Envelope(
-                        x1: 400177.58867753856,
-                        y1: 650887.0249267563,
-                        x2: 400367.92690205923,
-                        y2: 650988.133270391,
-                        SpatialReference.Create(27700)));
-                });
+                foreach (var annoTable in gdb.GeodatabaseAnnotationTables)
+                {
+                    Map.OperationalLayers.Add(new AnnotationLayer(annoTable));
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Geodatabase open failed: {e.Message}");
             }
         }
     }
